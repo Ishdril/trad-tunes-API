ee251ae86db9781f9afb8be0a1da386e1eac0737
Ishdril <berni2015@gmail.com>
2021-03-06 19:25:52 +0100

    added packages for logging and security

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,17 @@ require('dotenv').config();
 import router from './routes';
 import { MikroORM } from '@mikro-orm/core';
 import ormConfig from './orm.config';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const PORT = process.env.API_PORT || 3000;
 const app = express();
 
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 app.use(router);
 
 const main = async () => {
